77eeb47aa1d0c22fc2f26a0972330dc902d98506
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-02-12 18:15:32 -0300

    import function for geolocation API, add tests

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import "./style.css";
 import { apiKey } from "./modules/apiKeys";
-import { fetchWeather } from "./modules/fetch";
+import { fetchWeather, fetchGeocoding } from "./modules/fetch";
 
-fetchWeather(apiKey.openWeatherMap(), "london")
+fetchWeather("-34.61", "-58.38", "America%2FSao_Paulo")
+.then(weather => console.log(weather));
+
+fetchGeocoding(apiKey.openWeatherMap(), 10, "buenos aires")
 .then(weather => console.log(weather));
