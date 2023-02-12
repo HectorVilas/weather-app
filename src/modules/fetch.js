53ad779710a5f8b48126053ba64eeea81a7d0988
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-02-12 17:00:37 -0300

    API key as parameter for weather fetch function

--- src/modules/fetch.js
+++ src/modules/fetch.js
@@ -1,19 +1,10 @@
-export async function fetchWeather(city){
-  const scrambled = "64865b2ad14523a6cedd21bd516bf7b2";
-  const apiKey = decipher(scrambled);
-  const apiResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`);
-  const responseJson = await apiResponse.json();
-  
-  return responseJson;
-}
-
-function decipher(string){
-  let stringToArray = string.split("");
-  let unscrambled;
-
-  stringToArray.forEach(char => {
-    unscrambled = unscrambled ? char + unscrambled : char;
-  });
-  
-  return unscrambled;
+export async function fetchWeather(apiKey, city){
+  try {
+    const apiResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`);
+    const responseJson = await apiResponse.json();
+    
+    return responseJson;
+  } catch(err) {
+    console.warn("Something went wrong: \n" + err)
+  }
 }
