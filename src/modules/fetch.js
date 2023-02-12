688325761bec6c07d19f413eec56126f5bb06029
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-02-12 17:34:49 -0300

    add function to fetch city coordinates

--- src/modules/fetch.js
+++ src/modules/fetch.js
@@ -8,3 +8,14 @@ export async function fetchWeather(apiKey, city){
     console.warn("Something went wrong: \n" + err)
   }
 }
+
+export async function fetchGeocoding(apiKey, locationsLimit, location){
+  try {
+    const apiResponse = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=${locationsLimit}&appid=${apiKey}`);
+    const responseJson = await apiResponse.json();
+  
+    return responseJson;
+  } catch(err) {
+    console.warn("Something went wrong: \n" + err);
+  }
+}
