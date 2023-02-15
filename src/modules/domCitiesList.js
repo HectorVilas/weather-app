57905e6dc94a5653229a3b088ddbc882316ef997
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-02-14 22:09:45 -0300

    add cities list module
    This receives an array (from the geocoding result) and creates
    a new element with it's details, then returns a div with those
    elements.

---
+++ src/modules/domCitiesList.js
@@ -0,0 +1,24 @@
+import { fetchGeocoding } from "./fetch";
+import { countryCodeEmoji } from "country-code-emoji";
+import { getName } from "country-list";
+
+export async function domCitiesList(search){
+  const list = document.createElement("div");
+  const locations = await fetchGeocoding(10, search);
+  console.log(locations);
+  locations.forEach(location => {
+    const searchResult = document.createElement("div");
+    const flag = document.createElement("p");
+    const place = document.createElement("p");
+
+    searchResult.classList.add("search-result");
+    flag.classList.add("country-flag");
+    flag.innerText = countryCodeEmoji(location.country);
+    place.innerText = `${location.name}, ${location.state ? location.state + ", " : ""} ${getName(location.country)}`;
+
+    searchResult.append(flag, place);
+    list.append(searchResult);
+  });
+  
+  return list;
+}
