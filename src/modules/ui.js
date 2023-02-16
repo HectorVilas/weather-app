726d107ffdd699f4415219fd2a7267886c3d55d8
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-02-16 18:26:21 -0300

    import domCitiesList module, add listeners
    As I can't find a way to add listeners the way I want, the
    modules now only creates DOM elements and won't get listeners.
    Those listeners will be added directly from the UI module.
    A simple console.log has been added as event for testing.

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,8 +1,27 @@
 import { domSearch } from "./domSearch";
+import { domCitiesList } from "./domCitiesList";
 
 export function loadUi(){
   const body = document.querySelector("body");
 
   body.append(domSearch());
   
-}
+  const searchForm = document.querySelector(".search-form");
+  const searchInput = document.querySelector(".location-search input");
+  const cityList = document.querySelector(".search-results");
+  
+  searchForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const search = searchInput.value;
+    
+    cityList.replaceChildren();
+    cityList.append(await domCitiesList(search));
+
+    const domList = document.querySelectorAll(".search-result");
+    domList.forEach(city => {
+      city.addEventListener("click", () => {
+        console.log(city.dataset.latitude, city.dataset.longitude);
+      });
+    });
+  });
+};
