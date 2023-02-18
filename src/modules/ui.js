2e8c09275b246341c820bf4dd9ebf65991605757
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-02-18 18:40:56 -0300

    move code to function, timeout for keypress
    Now the search will fetch using keypress, but to prevent lots
    of fetching on each key press, a timeout will be reset if
    another key is pressed again before it ends.

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -3,30 +3,39 @@ import { domCitiesList } from "./domCitiesList";
 
 export function loadUi(){
   const body = document.querySelector("body");
+  let keydownTimeout;
 
   body.append(domSearch());
-  
-  const searchForm = document.querySelector(".search-form");
+
+  const searchInput = document.querySelector(".search-input");
+  searchInput.addEventListener("keypress", () => {
+    const searchInput = document.querySelector(".search-input");
+    const loadingIcon = document.querySelector(".search-loading");
+    searchInput.classList.add("loading");
+    loadingIcon.classList.remove("hidden");
+
+    clearTimeout(keydownTimeout);
+    keydownTimeout = setTimeout(() => {
+      searchCity(searchInput.value);
+    }, 750);
+  });
+};
+
+async function searchCity(search){
+  console.log("fetching");
   const searchInput = document.querySelector(".search-input");
   const cityList = document.querySelector(".search-results");
   const loadingIcon = document.querySelector(".search-loading");
-  
-  searchForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const search = searchInput.value;
-    searchInput.classList.add("loading");
-    loadingIcon.classList.remove("hidden");
-    const list = await domCitiesList(search);
-    searchInput.classList.remove("loading");
-    loadingIcon.classList.add("hidden");
-    cityList.replaceChildren();
-    cityList.append(list);
+  const list = await domCitiesList(search);
+  searchInput.classList.remove("loading");
+  loadingIcon.classList.add("hidden");
+  cityList.replaceChildren();
+  cityList.append(list);
 
-    const domList = document.querySelectorAll(".search-result");
-    domList.forEach(city => {
-      city.addEventListener("click", () => {
-        console.log(city.dataset.latitude, city.dataset.longitude);
-      });
+  const domList = document.querySelectorAll(".search-result");
+  domList.forEach(city => {
+    city.addEventListener("click", () => {
+      console.log(city.dataset.latitude, city.dataset.longitude);
     });
   });
 };
