9ddd1e369c301980c90b6190e413b038aa1e952a
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-02-18 01:26:51 -0300

    add divs for loading icon

--- src/modules/domSearch.js
+++ src/modules/domSearch.js
@@ -3,15 +3,21 @@ export function domSearch(){
   const form = document.createElement("form");
   const input = document.createElement("input");
   const cityList = document.createElement("div");
+  const loadingAnimDiv = document.createElement("div");
+  const loadingAnim1 = document.createElement("div");
+  const loadingAnim2 = document.createElement("div");
+  const loadingAnim3 = document.createElement("div");
   
   search.classList.add("location-search");
   form.classList.add("search-form");
   cityList.classList.add("search-results");
+  loadingAnimDiv.classList.add("search-loading", "hidden");
   input.classList.add("search-input");
   input.type = "search";
   input.placeholder = "Write a location name";
   
-  form.append(input);
+  loadingAnimDiv.append(loadingAnim1, loadingAnim2, loadingAnim3)
+  form.append(input, loadingAnimDiv);
   search.append(form, cityList);
 
   return search;
