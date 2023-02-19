5e4a77e402b2d33ba41bea3b9fc91d1636677a16
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-02-19 14:44:30 -0300

    move searchCity function to own module

---
+++ src/modules/functions/searchCity.js
@@ -0,0 +1,29 @@
+import { domCitiesList } from "../domCitiesList";
+
+let previousSearch;
+
+export async function searchCity(search){
+  const searchInput = document.querySelector(".search-input");
+  const cityList = document.querySelector(".search-results");
+  const loadingIcon = document.querySelector(".search-loading");
+  
+  if(search == previousSearch) {
+    searchInput.classList.remove("loading");
+    loadingIcon.classList.add("hidden");
+    return;
+  };
+  
+  previousSearch = search;
+  const list = await domCitiesList(search);
+  searchInput.classList.remove("loading");
+  loadingIcon.classList.add("hidden");
+  cityList.replaceChildren();
+  cityList.append(list);
+
+  const domList = document.querySelectorAll(".search-result");
+  domList.forEach(city => {
+    city.addEventListener("click", () => {
+      console.log(city.dataset.latitude, city.dataset.longitude);
+    });
+  });
+};
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,7 +1,5 @@
 import { domSearch } from "./domSearch";
-import { domCitiesList } from "./domCitiesList";
-
-let previousSearch;
+import { searchCity } from "./functions/searchCity";
 
 export function loadUi(){
   const body = document.querySelector("body");
@@ -29,29 +27,3 @@ export function loadUi(){
   });
 };
 
-async function searchCity(search){
-  console.log(1);
-  const searchInput = document.querySelector(".search-input");
-  const cityList = document.querySelector(".search-results");
-  const loadingIcon = document.querySelector(".search-loading");
-  
-  if(search == previousSearch) {
-    searchInput.classList.remove("loading");
-    loadingIcon.classList.add("hidden");
-    return;
-  };
-  
-  previousSearch = search;
-  const list = await domCitiesList(search);
-  searchInput.classList.remove("loading");
-  loadingIcon.classList.add("hidden");
-  cityList.replaceChildren();
-  cityList.append(list);
-
-  const domList = document.querySelectorAll(".search-result");
-  domList.forEach(city => {
-    city.addEventListener("click", () => {
-      console.log(city.dataset.latitude, city.dataset.longitude);
-    });
-  });
-};
