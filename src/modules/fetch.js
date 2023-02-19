e007144bc42342c29e06650b8b993f180ba55ef6
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-02-19 20:10:48 -0300

    remove comment with coordinates example

--- src/modules/fetch.js
+++ src/modules/fetch.js
@@ -2,11 +2,6 @@ import { getApiKey } from "./apiKeys";
 
 export async function fetchWeather(latitude, longitude, timezone = "auto"){
   try {
-    /**
-    * for Buenos Aires, Argentina:
-    * &latitude=-34.61&longitude=-58.38
-    * &timezone=America%2FSao_Paulo
-    */
     const apiResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,weathercode,cloudcover,visibility,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_sum,precipitation_hours,windspeed_10m_max,winddirection_10m_dominant&timezone=${timezone}&current_weather=true`);
     const responseJson = await apiResponse.json();
     
