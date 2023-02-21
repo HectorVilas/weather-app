82a1b394f88471b1ade90e4d24c22f1d02dfcb00
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-02-21 18:51:01 -0300

    simplify weathercode code

--- src/modules/functions/weathercode.js
+++ src/modules/functions/weathercode.js
@@ -1,51 +1,61 @@
-export default function getWeathercode(code) {
+export default function getWeathercode(requestedCode) {
   // source: https://open-meteo.com/en/docs
   const codes = [
-    'Clear sky',
-    'Mainly clear, partly cloudy, and overcast',
-    'Fog and depositing rime fog',
-    'Drizzle: Light, moderate, and dense intensity',
-    'Freezing Drizzle: Light and dense intensity',
-    'Rain: Slight, moderate and heavy intensity',
-    'Freezing Rain: Light and heavy intensity',
-    'Snow fall: Slight, moderate, and heavy intensity',
-    'Snow grains',
-    'Rain showers: Slight, moderate, and violent',
-    'Snow showers slight and heavy',
-    'Thunderstorm: Slight or moderate',
-    'Thunderstorm with slight and heavy hail',
+    {
+      codes: [0],
+      quote: 'Clear sky',
+    },
+    {
+      codes: [1, 2, 3],
+      quote: 'Mainly clear, partly cloudy, and overcast',
+    },
+    {
+      codes: [45, 48],
+      quote: 'Fog and depositing rime fog',
+    },
+    {
+      codes: [51, 53, 55],
+      quote: 'Drizzle: Light, moderate, and dense intensity',
+    },
+    {
+      codes: [56, 57],
+      quote: 'Freezing Drizzle: Light and dense intensity',
+    },
+    {
+      codes: [61, 63, 65],
+      quote: 'Rain: Slight, moderate and heavy intensity',
+    },
+    {
+      codes: [66, 67],
+      quote: 'Freezing Rain: Light and heavy intensity',
+    },
+    {
+      codes: [71, 73, 75],
+      quote: 'Snow fall: Slight, moderate, and heavy intensity',
+    },
+    {
+      codes: [77],
+      quote: 'Snow grains',
+    },
+    {
+      codes: [80, 81, 82],
+      quote: 'Rain showers: Slight, moderate, and violent',
+    },
+    {
+      codes: [85, 86],
+      quote: 'Snow showers slight and heavy',
+    },
+    {
+      codes: [95],
+      quote: 'Thunderstorm: Slight or moderate',
+    },
+    {
+      codes: [96, 99],
+      quote: 'Thunderstorm with slight and heavy hail',
+    },
   ];
 
-  const weathercode = {
-    0: codes[0],
-    1: codes[1],
-    2: codes[1],
-    3: codes[1],
-    45: codes[2],
-    48: codes[2],
-    51: codes[3],
-    53: codes[3],
-    55: codes[3],
-    56: codes[4],
-    57: codes[4],
-    61: codes[5],
-    63: codes[5],
-    65: codes[5],
-    66: codes[6],
-    67: codes[6],
-    71: codes[7],
-    73: codes[7],
-    75: codes[7],
-    77: codes[8],
-    80: codes[9],
-    81: codes[9],
-    82: codes[9],
-    85: codes[10],
-    86: codes[10],
-    95: codes[11],
-    96: codes[12],
-    99: codes[12],
-  };
+  const requested = codes.findIndex((item) => item.codes.includes(requestedCode));
 
-  return weathercode[code];
+  return codes[requested];
 }
