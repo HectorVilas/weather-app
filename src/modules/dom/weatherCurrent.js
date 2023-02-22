6558c90b70d1dda89b88c91fd503a276168a6a8a
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-02-22 17:12:51 -0300

    add weathercode and humidity, organize elements

--- src/modules/dom/weatherCurrent.js
+++ src/modules/dom/weatherCurrent.js
@@ -1,31 +1,77 @@
 export default function weatherCurrent() {
   const section = document.createElement('section');
+
+  // icon
   const icon = document.createElement('div');
+  section.classList.add('weather-current');
+  icon.classList.add('icon');
+
+  // weathercode div
+  const weathercodeDiv = document.createElement('div');
+  const weathercodePara = document.createElement('p');
+
+  weathercodeDiv.classList.add('weathercode-div');
+  weathercodePara.classList.add('weathercode-para');
+  weathercodePara.innerText = 'Mainly clear';
+
+  weathercodeDiv.append(weathercodePara);
+
+  // temperatures div
   const tempDiv = document.createElement('div');
   const tempCurrent = document.createElement('p');
   const tempUnit = document.createElement('input');
-  const apparentTemp = document.createElement('p');
+  const apparentPara = document.createElement('p');
+  const apparentTempSpan = document.createElement('span');
+  const apparentTemp = document.createElement('span');
+  const apparentTempUnitSpan = document.createElement('span');
+
+  tempDiv.classList.add('temp-div');
+  tempCurrent.classList.add('temp-current');
+  tempUnit.classList.add('temp-unit');
+  tempUnit.type = 'checkbox';
+  apparentPara.classList.add('temp-apparent-para');
+  apparentTemp.classList.add('temp-apparent');
+
+  tempCurrent.innerText = '000';
+  apparentTempSpan.innerText = 'Feels like: ';
+  apparentTemp.innerText = '000';
+  apparentTempUnitSpan.innerText = '°';
+
+  apparentPara.append(apparentTempSpan, apparentTemp, apparentTempUnitSpan);
+  tempDiv.append(tempCurrent, tempUnit, apparentPara);
+
+  // humidity div
+  const humidityDiv = document.createElement('div');
+  const humidityPara = document.createElement('p');
+  const humidityText = document.createElement('span');
+  const humidity = document.createElement('span');
+  const humidityPercent = document.createElement('span');
+
+  humidityDiv.classList.add('humidity-div');
+  humidityPara.classList.add('humidity-para');
+  humidity.classList.add('humidity');
+
+  humidityText.innerText = 'Humidity: ';
+  humidity.innerText = '000';
+  humidityPercent.innerText = '%';
+
+  humidityPara.append(humidityText, humidity, humidityPercent);
+  humidityDiv.append(humidityPara);
+
+  // wind div
   const windDiv = document.createElement('div');
   const windSpeed = document.createElement('p');
   const windSpeedUnit = document.createElement('input');
   const windSpeedDirection = document.createElement('div');
 
-  section.classList.add('weather-current');
-  icon.classList.add('icon');
-  tempDiv.classList.add('temp-div');
-  tempCurrent.classList.add('temp-current');
-  tempUnit.classList.add('temp-unit');
-  tempUnit.type = 'checkbox';
-  apparentTemp.classList.add('temp-apparent');
   windDiv.classList.add('wind-div');
   windSpeed.classList.add('wind-speed');
   windSpeedUnit.classList.add('speed-unit');
   windSpeedUnit.type = 'checkbox';
   windSpeedDirection.classList.add('wind-direction');
 
-  tempDiv.append(tempCurrent, tempUnit, apparentTemp);
   windDiv.append(windSpeed, windSpeedUnit, windSpeedDirection);
-  section.append(icon, tempDiv, windDiv);
+  section.append(icon, weathercodeDiv, tempDiv, humidityDiv, windDiv);
 
   return section;
 }
