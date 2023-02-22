4975b559ed08d7f00937ed6c23bf374ae20258ba
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-02-22 15:43:12 -0300

    add current weather dom element creation

---
+++ src/modules/dom/weatherCurrent.js
@@ -0,0 +1,31 @@
+export default function weatherCurrent() {
+  const section = document.createElement('section');
+  const icon = document.createElement('div');
+  const tempDiv = document.createElement('div');
+  const tempCurrent = document.createElement('p');
+  const tempUnit = document.createElement('input');
+  const apparentTemp = document.createElement('p');
+  const windSpeedDiv = document.createElement('div');
+  const windSpeed = document.createElement('p');
+  const windSpeedUnit = document.createElement('input');
+  const windSpeedDirection = document.createElement('div');
+
+  section.classList.add('weather-current');
+  icon.classList.add('icon');
+  tempDiv.classList.add('temp-div');
+  tempCurrent.classList.add('temp-current');
+  tempUnit.classList.add('temp-unit');
+  tempUnit.type = 'checkbox';
+  apparentTemp.classList.add('temp-apparent');
+  windSpeedDiv.classList.add('wind-speed-div');
+  windSpeed.classList.add('wind-speed');
+  windSpeedUnit.classList.add('speed-unit');
+  windSpeedUnit.type = 'checkbox';
+  windSpeedDirection.classList.add('wind-direction');
+
+  tempDiv.append(tempCurrent, tempUnit, apparentTemp);
+  windSpeedDiv.append(windSpeed, windSpeedUnit, windSpeedDirection);
+  section.append(icon, tempDiv, windSpeedDiv);
+
+  return section;
+}
