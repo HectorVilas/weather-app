c3f58b18f4972dfdb86a78a94cb90bb08b7c1c66
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-02-23 16:29:59 -0300

    add location as dataset

--- src/modules/dom/citiesList.js
+++ src/modules/dom/citiesList.js
@@ -24,13 +24,15 @@ export default async function domCitiesList(search) {
     const searchResult = document.createElement('div');
     const flag = document.createElement('p');
     const place = document.createElement('p');
+    const location = `${city.name}, ${city.admin3 ? `${city.admin3}, ` : ''}${city.admin2 ? `${city.admin2}, ` : ''}${city.admin1 ? `${city.admin1}, ` : ''}${city.country}`;
 
     searchResult.classList.add('search-result');
     searchResult.dataset.latitude = city.latitude;
     searchResult.dataset.longitude = city.longitude;
+    searchResult.dataset.location = location;
     flag.classList.add('country-flag');
     flag.innerText = countryCodeEmoji(city.country_code);
-    place.innerText = `${city.name}, ${city.admin3 ? `${city.admin3}, ` : ''}${city.admin2 ? `${city.admin2}, ` : ''}${city.admin1 ? `${city.admin1}, ` : ''} ${city.country}`;
+    place.innerText = location;
 
     searchResult.append(flag, place);
     list.append(searchResult);
