a7c47d0e54f4ecdf156a49fcb37cbc9b2a6daf7a
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-03-04 16:47:32 -0300

    return number with max one decimal

--- src/modules/functions/unitConverter.js
+++ src/modules/functions/unitConverter.js
@@ -1,9 +1,11 @@
 export function celsiusToFahrenheit(number) {
   const fahrenheit = (number * 1.8) + 32;
-  return fahrenheit;
+  const fixedDecimals = parseFloat(parseFloat(fahrenheit).toFixed(1));
+  return fixedDecimals;
 }
 
 export function kilometersToMiles(number) {
   const miles = number / 1.609344;
-  return miles;
+  const fixedDecimals = parseFloat(parseFloat(miles).toFixed(1));
+  return fixedDecimals;
 }
