663955e57240a7414fc5607980a34a865cafebea
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-03-03 22:24:06 -0300

    add module to convert temperature and speed units

---
+++ src/modules/functions/unitConverter.js
@@ -0,0 +1,9 @@
+export function celsiusToFahrenheit(number) {
+  const fahrenheit = (number * 1.8) + 32;
+  return fahrenheit;
+}
+
+export function kilometersToMiles(number) {
+  const miles = number / 1.609344;
+  return miles;
+}
