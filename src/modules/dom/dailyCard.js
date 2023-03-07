a382b1c9812481fe8baaadf7988e14987dd74098
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-03-07 20:04:00 -0300

    add function to generate weather icon and temps

--- src/modules/dom/dailyCard.js
+++ src/modules/dom/dailyCard.js
@@ -8,3 +8,43 @@ export default function dailyCard() {
 
   return card;
 }
+
+function weatherDiv() {
+  const div = document.createElement('div');
+  const day = document.createElement('p');
+  const weatherIcon = document.createElement('div');
+  const temp = document.createElement('p');
+  const tempUnit = document.createElement('input');
+  const tempApparent = document.createElement('p');
+  const tempApparentUnit = document.createElement('input');
+  const weatherDescription = document.createElement('p');
+
+  div.classList.add('daily-weather');
+  day.classList.add('daily-day');
+  weatherIcon.classList.add('weather-current-icon', 'daily-weather-icon');
+  temp.classList.add('daily-temp', 'temperature-number');
+  tempUnit.classList.add('daily-temp-unit', 'gauge-unit', 'temperature-unit');
+  tempUnit.type = 'checkbox';
+  tempApparent.classList.add('daily-temp-apparent', 'temperature-number');
+  tempApparentUnit.classList.add('daily-temp-apparent-unit', 'gauge-unit', 'temperature-unit');
+  tempApparentUnit.type = 'checkbox';
+  weatherDescription.classList.add('daily-weather-description');
+
+  day.innerText = 'day placeholder';
+  weatherIcon.style.background = 'blue';
+  temp.innerText = '20';
+  tempApparent.innerText = '18';
+  weatherDescription.innerText = 'weather placeholder';
+
+  div.append(
+    day,
+    weatherIcon,
+    temp,
+    tempUnit,
+    tempApparent,
+    tempApparentUnit,
+    weatherDescription,
+  );
+
+  return div;
+}
