629fc25ac8e76ca128e0969535fd4677b9118baf
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-03-07 18:30:18 -0300

    import weather card module, append for testing

--- src/modules/dom/weatherDaily.js
+++ src/modules/dom/weatherDaily.js
@@ -1,7 +1,11 @@
+import dailyCard from './dailyCard';
+
 export default function weatherDaily() {
   const section = document.createElement('section');
 
   section.classList.add('weather-daily');
+
+  section.append(dailyCard());
 
   return section;
 }
