a0f508490ebedf87bebec49f3c6887a66e4f4a5c
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-03-08 17:40:17 -0300

    move temp values and units to individual divs

--- src/modules/dom/dailyCard.js
+++ src/modules/dom/dailyCard.js
@@ -13,8 +13,10 @@ function weatherDiv() {
   const div = document.createElement('div');
   const day = document.createElement('p');
   const weatherIcon = document.createElement('div');
+  const tempDiv = document.createElement('div');
   const temp = document.createElement('p');
   const tempUnit = document.createElement('input');
+  const tempApparentDiv = document.createElement('div');
   const tempApparent = document.createElement('p');
   const tempApparentUnit = document.createElement('input');
   const weatherDescription = document.createElement('p');
@@ -22,9 +24,11 @@ function weatherDiv() {
   div.classList.add('daily-weather');
   day.classList.add('daily-day');
   weatherIcon.classList.add('weather-current-icon', 'daily-weather-icon');
+  tempDiv.classList.add('daily-temp-div');
   temp.classList.add('daily-temp', 'temperature-number');
   tempUnit.classList.add('daily-temp-unit', 'gauge-unit', 'temperature-unit');
   tempUnit.type = 'checkbox';
+  tempApparentDiv.classList.add('daily-temp-apparent-div');
   tempApparent.classList.add('daily-temp-apparent', 'temperature-number');
   tempApparentUnit.classList.add('daily-temp-apparent-unit', 'gauge-unit', 'temperature-unit');
   tempApparentUnit.type = 'checkbox';
@@ -32,17 +36,17 @@ function weatherDiv() {
 
   day.innerText = 'day placeholder';
   weatherIcon.style.background = 'blue';
-  temp.innerText = '20';
-  tempApparent.innerText = '18';
-  weatherDescription.innerText = 'weather placeholder';
+  temp.innerText = '000.0';
+  tempApparent.innerText = '000.0';
+  weatherDescription.innerText = 'weather description';
 
+  tempDiv.append(temp, tempUnit);
+  tempApparentDiv.append(tempApparent, tempApparentUnit);
   div.append(
     day,
     weatherIcon,
-    temp,
-    tempUnit,
-    tempApparent,
-    tempApparentUnit,
+    tempDiv,
+    tempApparentDiv,
     weatherDescription,
   );
 
