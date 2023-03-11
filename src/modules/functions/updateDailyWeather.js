d695d136726b0e5062f644b15b4f7264a1345bf1
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-03-11 13:37:24 -0300

    add datasets to speed units, allow conversion

--- src/modules/functions/updateDailyWeather.js
+++ src/modules/functions/updateDailyWeather.js
@@ -57,7 +57,10 @@ export default function updateDailyWeather(data) {
     hours.innerText = `${data.precipitationHours[i]}hs`;
   });
   windSpeeds.forEach((speed, i) => {
-    speed.innerText = data.windSpeeds[i];
+    const windSpeed = data.windSpeeds[i];
+    speed.innerText = windSpeed;
+    speed.dataset.kilometers = windSpeed;
+    speed.dataset.miles = kilometersToMiles(windSpeed);
   });
   sunrises.forEach((sunrise, i) => {
     sunrise.innerText = `${getHours(new Date(data.sunrises[i]))}:${getMinutes(new Date(data.sunrises[i]))}hs`;
