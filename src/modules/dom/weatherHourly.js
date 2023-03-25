b752f5637875e8d7e2f0fa1c0cbad2ef7826c32d
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-03-25 19:29:00 -0300

    add svg object for hourly weather chart

--- src/modules/dom/weatherHourly.js
+++ src/modules/dom/weatherHourly.js
@@ -1,6 +1,21 @@
 export default function weatherHourly() {
   const div = document.createElement('div');
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  const tempChart = document.createElementNS('http://www.w3.org/2000/svg', 'path');
 
   div.classList.add('weather-hourly');
+  svg.classList.add('hourly-chart');
+  tempChart.classList.add('hourly-chart-temperature');
+  svg.setAttribute('width', '100%');
+  svg.setAttribute('height', '100%');
+  tempChart.setAttribute('d', 'M 10 10 L 110 10 L 60 70 Z');
+  tempChart.setAttribute('fill', 'transparent');
+  tempChart.setAttribute('stroke', 'red');
+  tempChart.setAttribute('stroke-width', '5');
+  tempChart.setAttribute('stroke-linecap', 'round');
+  tempChart.setAttribute('stroke-linejoin', 'round');
+
+  svg.append(tempChart);
+  div.append(svg);
   return div;
 }
