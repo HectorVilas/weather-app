03d6ca9959afb3a5fb61ef6e48e6e68b89ee693b
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-03-26 18:33:08 -0300

    move code for temp chart to function

--- src/modules/functions/updateHourlyWeather.js
+++ src/modules/functions/updateHourlyWeather.js
@@ -1,6 +1,5 @@
 export default function updateHourlyWeather(data) {
   const parent = document.querySelector('.weather-hourly');
-  const tempChart = document.querySelector('.hourly-chart-temperature');
   const hours = 24;
   const marginX = 20;
   const width = parseInt(parent.clientWidth, 10) - marginX;
@@ -11,10 +10,23 @@ export default function updateHourlyWeather(data) {
     const currentValue = parseFloat(parseFloat(((width) / (hours - 1)) * (i + 1)).toFixed(1));
     positionsX.push(currentValue);
   }
+
+  updateTemperature(data.temps, marginX, positionsX, hours);
+}
+
+function updateTemperature(temps, marginX, positionsX, hours) {
+  const tempChart = document.querySelector('.hourly-chart-temperature');
+
+  const positionsY = [];
+  // set values for vector vertical positions
+  for (let i = 0; i < hours; i += 1) {
+    const currentValue = parseFloat(temps[i]);
+    positionsY.push(currentValue);
+  }
   // use values on draw
   let drawTemp = '';
   positionsX.forEach((pos, i) => {
-    drawTemp += `${i === 0 ? 'M' : 'L'} ${pos + marginX / 2} ${i % 2 ? 50 : 10}`;
+    drawTemp += ` ${i === 0 ? 'M' : 'L'} ${pos + marginX / 2} ${positionsY[i]}`;
   });
 
   tempChart.setAttribute('d', drawTemp);
