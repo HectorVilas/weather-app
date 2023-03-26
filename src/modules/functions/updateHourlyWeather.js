a8d875c804b7529587216c27d3c0052ced8ffefa
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-03-25 21:40:00 -0300

    add path placeholder
    This is the formula for calculating each vector horizontally,
    respecting the margin. The vertical position is just a zig-zag
    for visualization, it will be based on temperatures.

--- src/modules/functions/updateHourlyWeather.js
+++ src/modules/functions/updateHourlyWeather.js
@@ -1,3 +1,21 @@
 export default function updateHourlyWeather(data) {
-  console.log(data);
+  const parent = document.querySelector('.weather-hourly');
+  const tempChart = document.querySelector('.hourly-chart-temperature');
+  const hours = 24;
+  const marginX = 20;
+  const width = parseInt(parent.clientWidth, 10) - marginX;
+  const height = parseInt(parent.clientHeight, 10);
+  const positionsX = [0];
+  // set values for vector horizontal positions
+  for (let i = 0; i < hours - 1; i += 1) {
+    const currentValue = parseFloat(parseFloat(((width) / (hours - 1)) * (i + 1)).toFixed(1));
+    positionsX.push(currentValue);
+  }
+  // use values on draw
+  let drawTemp = '';
+  positionsX.forEach((pos, i) => {
+    drawTemp += `${i === 0 ? 'M' : 'L'} ${pos + marginX / 2} ${i % 2 ? 50 : 10}`;
+  });
+
+  tempChart.setAttribute('d', drawTemp);
 }
