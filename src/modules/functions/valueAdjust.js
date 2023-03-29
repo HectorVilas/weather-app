f90466a04216744c5bfcfed86adfc68396f326d4
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-03-29 20:17:21 -0300

    remove old, commented code

--- src/modules/functions/valueAdjust.js
+++ src/modules/functions/valueAdjust.js
@@ -8,21 +8,3 @@ export default function valueAdjust(domElement, fromValue, toValue) {
     }, 25 * (i + 1));
   }
 }
-
-// export default function valueAdjust(domElement, fromValue, toValue) {
-//   const timeout = 20;
-//   let step;
-//   if (domElement.className.includes('temperature-number')) step = 0.1;
-//   if (domElement.className.includes('humidity')
-//   || domElement.className.includes('speed-number')) step = 1;
-//   const fDomValue = parseFloat(domElement.innerText);
-//   const fFromValue = parseFloat(fromValue);
-//   const fToValue = parseFloat(toValue);
-//   const increment = fFromValue < fToValue ? step : step * -1;
-//   if (fDomValue !== fToValue) {
-//     domElement.innerText = parseFloat((fDomValue + increment).toFixed(1));
-//     setTimeout(() => {
-//       valueAdjust(domElement, fromValue, toValue);
-//     }, timeout);
-//   }
-// }
