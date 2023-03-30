ef0e8c873711b30f5bfe74b71ff878faa2cba289
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-03-30 13:38:34 -0300

    move temp chart to function

--- src/modules/dom/weatherHourly.js
+++ src/modules/dom/weatherHourly.js
@@ -1,6 +1,14 @@
 export default function weatherHourly() {
   const div = document.createElement('div');
-  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  div.classList.add('weather-hourly');
+  div.append(
+    createTempGroup(),
+  );
+  return div;
+}
+
+function createTempGroup() {
+  const tempSvg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
   const tempGroup = document.createElementNS('http://www.w3.org/2000/svg', 'g');
   const tempChart = document.createElementNS('http://www.w3.org/2000/svg', 'path');
   const tempChartNumbers = [];
@@ -19,12 +27,11 @@ export default function weatherHourly() {
     tempChartNumbers.push(degrees);
   }
 
-  div.classList.add('weather-hourly');
-  svg.classList.add('hourly-chart');
+  tempSvg.classList.add('hourly-chart');
   tempGroup.classList.add('hourly-chart-group');
   tempChart.classList.add('hourly-chart-temperature');
-  svg.setAttribute('width', '100%');
-  svg.setAttribute('height', '100%');
+  tempSvg.setAttribute('width', '100%');
+  tempSvg.setAttribute('height', '100%');
   tempChart.setAttribute('fill', 'transparent');
   tempChart.setAttribute('stroke', 'red');
   tempChart.setAttribute('stroke-width', '5');
@@ -32,7 +39,7 @@ export default function weatherHourly() {
   tempChart.setAttribute('stroke-linejoin', 'round');
 
   tempGroup.append(tempChart, ...tempChartNumbers);
-  svg.append(tempGroup);
-  div.append(svg);
-  return div;
+  tempSvg.append(tempGroup);
+
+  return tempSvg;
 }
