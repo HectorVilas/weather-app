37223288f89169451d1162ae26f95875f2ff4e6f
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-01 16:38:30 -0300

    add 7 days of data to parameter instead of 1 day

--- src/modules/functions/searchCity.js
+++ src/modules/functions/searchCity.js
@@ -65,16 +65,16 @@ export default async function searchCity(search) {
         sunsets: weather.daily.sunset,
         uvIndexes: weather.daily.uv_index_max,
       };
-      // data for daily weather for 24 hours of current day, then update
-      const localHour = new Date(weather.current_weather.time).getHours();
       updateDailyWeather(dailyWeatherData);
+      // data for hourly weather for 24 hours of current day, then update
+      // const localHour = new Date(weather.current_weather.time).getHours();
       const hourlyWeatherData = {
-        hours: getNext24(weather.hourly.time, localHour),
-        temps: getNext24(weather.hourly.temperature_2m, localHour),
-        humidity: getNext24(weather.hourly.relativehumidity_2m, localHour),
-        apparent: getNext24(weather.hourly.apparent_temperature, localHour),
-        weathercode: getNext24(weather.hourly.weathercode, localHour),
-        windspeed: getNext24(weather.hourly.windspeed_10m, localHour),
+        hours: weather.hourly.time,
+        temps: weather.hourly.temperature_2m,
+        humidity: weather.hourly.relativehumidity_2m,
+        apparent: weather.hourly.apparent_temperature,
+        weathercode: weather.hourly.weathercode,
+        windspeed: weather.hourly.windspeed_10m,
       };
       updateHourlyWeather(hourlyWeatherData);
       hideSearch();
@@ -89,11 +89,11 @@ function hideSearch() {
   domCitiesList('');
 }
 
-function getNext24(array, localHour) {
-  const newArray = [];
-  for (let i = 0; i < 24; i += 1) {
-    newArray.push(array[i + localHour]);
-  }
+// function getNext24(array, localHour) {
+//   const newArray = [];
+//   for (let i = 0; i < 24; i += 1) {
+//     newArray.push(array[i + localHour]);
+//   }
 
-  return newArray;
-}
+//   return newArray;
+// }
