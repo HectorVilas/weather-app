a0b70c4f8b738839c880fc69e92f0aca114c6532
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-02 20:47:32 -0300

    remove obsolete module
    As the way to work with the line chart changed, this module
    won't be useful anymore.

--- src/modules/functions/moveViewBox.js
+++
@@ -1,6 +0,0 @@
-export default function moveViewBox(domParent, svg, startFromHour = 0, startFromDay = 0) {
-  const width = parseInt(domParent.clientWidth, 10);
-  const height = parseInt(domParent.clientHeight, 10);
-  const viewboxStart = ((width / 24) * startFromHour) + (width * startFromDay);
-  svg.setAttribute('viewBox', `${viewboxStart} ${0} ${width} ${height}`);
-}
