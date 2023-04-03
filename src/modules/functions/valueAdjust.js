839f2c887f8229cd31e4b04e6f44959b2371c8c4
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-03 18:21:39 -0300

    allow selecting steps
    This way the duration can be longer or shorter. The parameter
    is optional, 50 by default as previously was.

--- src/modules/functions/valueAdjust.js
+++ src/modules/functions/valueAdjust.js
@@ -1,10 +1,9 @@
-export default function valueAdjust(domElement, fromValue, toValue) {
-  const quantity = 50;
+export default function valueAdjust(domElement, fromValue, toValue, steps = 50) {
   const isFloat = domElement.classList.contains('temperature-number') ? 1 : 0;
-  for (let i = 0; i < 50; i += 1) {
+  for (let i = 0; i < steps; i += 1) {
     setTimeout(() => {
-      const currentValue = parseFloat(fromValue) + (((toValue - fromValue) / quantity) * (i + 1));
-      domElement.textContent = i === quantity - 1 ? toValue : currentValue.toFixed(isFloat);
+      const currentValue = parseFloat(fromValue) + (((toValue - fromValue) / steps) * (i + 1));
+      domElement.textContent = i === steps - 1 ? toValue : currentValue.toFixed(isFloat);
     }, 25 * (i + 1));
   }
 }
