1154f5b46113b7676a299d28dae3ba43a473f046
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-04 18:53:37 -0300

    move click action to module

--- src/modules/search/searchCity.js
+++ src/modules/search/searchCity.js
@@ -1,9 +1,5 @@
 import domCitiesList from './citiesList';
-import { fetchWeather } from '../fetch';
-import updateCurrentWeather from '../weatherCurrent/updateCurrentWeather';
-import updateDailyWeather from '../weatherDailyHourly/weatherDaily/updateDailyWeather';
-import updateHourlyWeather from '../weatherDailyHourly/weatherHourly/updateHourlyWeather';
-import getWeathercode from '../weathercode';
+import updateCompleteWeather from './updateCompleteWeather';
 
 let previousSearch;
 
@@ -27,58 +23,15 @@ export default async function searchCity(search) {
 
   const domList = document.querySelectorAll('.search-result');
   domList.forEach((city) => {
-    city.addEventListener('click', async () => {
+    city.addEventListener('click', () => {
+      cityList.replaceChildren();
       // disable and autocomplete input while fetching data
       searchInput.setAttribute('disabled', true);
       searchInput.value = city.dataset.location;
       searchInput.classList.add('loading');
       loadingIcon.classList.remove('hidden');
 
-      cityList.replaceChildren();
-      const { latitude } = city.dataset;
-      const { longitude } = city.dataset;
-      const weather = await fetchWeather(latitude, longitude);
-      const currentHour = new Date().getHours();
-      const currentWeatherData = {
-        city: city.dataset.location,
-        weathercode: getWeathercode(weather.current_weather.weathercode),
-        temp: weather.current_weather.temperature,
-        tempApparent: weather.hourly.apparent_temperature[currentHour],
-        humidity: weather.hourly.relativehumidity_2m[currentHour],
-        windSpeed: weather.current_weather.windspeed,
-        windDirection: weather.current_weather.winddirection,
-        sunrise: weather.daily.sunrise,
-        sunset: weather.daily.sunset,
-        localHour: weather.current_weather.time,
-      };
-      const dailyWeatherData = {
-        weathercodes: weather.daily.weathercode,
-        tempsMax: weather.daily.temperature_2m_max,
-        tempsMin: weather.daily.temperature_2m_min,
-        precipitationQuantities: weather.daily.precipitation_sum,
-        precipitationHours: weather.daily.precipitation_hours,
-        windSpeeds: weather.daily.windspeed_10m_max,
-        sunrises: weather.daily.sunrise,
-        sunsets: weather.daily.sunset,
-        uvIndexes: weather.daily.uv_index_max,
-      };
-      const hourlyWeatherData = {
-        localHour: new Date(weather.current_weather.time).getHours(),
-        hours: weather.hourly.time,
-        temps: weather.hourly.temperature_2m,
-        humidity: weather.hourly.relativehumidity_2m,
-        apparent: weather.hourly.apparent_temperature,
-        weathercode: weather.hourly.weathercode,
-        windspeed: weather.hourly.windspeed_10m,
-      };
-      updateCurrentWeather(currentWeatherData);
-      updateDailyWeather(dailyWeatherData);
-      updateHourlyWeather(hourlyWeatherData, hourlyWeatherData.localHour);
-      // hide search
-      const domSearch = document.querySelector('.city-search');
-      domSearch.classList.add('hidden');
-      // clear results after hiding
-      domCitiesList('');
+      updateCompleteWeather(city);
     });
   });
 }
