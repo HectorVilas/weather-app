15acb9ae97d92659f69df49cb9e56731e57514ca
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-06 14:11:05 -0300

    add CORS mode to fetch

--- src/modules/fetch.js
+++ src/modules/fetch.js
@@ -1,7 +1,9 @@
 export async function fetchWeather(latitude, longitude, timezone = 'auto') {
   let responseJson;
   try {
-    const apiResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,weathercode,cloudcover,visibility,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_sum,precipitation_hours,windspeed_10m_max,winddirection_10m_dominant&timezone=${timezone}&current_weather=true`);
+    const apiResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,weathercode,cloudcover,visibility,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_sum,precipitation_hours,windspeed_10m_max,winddirection_10m_dominant&timezone=${timezone}&current_weather=true`, {
+      mode: 'cors',
+    });
     responseJson = await apiResponse.json();
   } catch (err) {
     responseJson = `Something went wrong: \n${err}`;
