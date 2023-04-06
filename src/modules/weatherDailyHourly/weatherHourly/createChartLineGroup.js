ca9aa99b51f1925679d2e3ad3a74e1fe6079ee8e
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-06 15:18:33 -0300

    move function to own module

---
+++ src/modules/weatherDailyHourly/weatherHourly/createChartLineGroup.js
@@ -0,0 +1,41 @@
+export default function createChartLineGroup(className, unitSymbol, unitType) {
+  const svgNs = 'http://www.w3.org/2000/svg';
+  const chartGroup = document.createElementNS(svgNs, 'g');
+  const chart = document.createElementNS(svgNs, 'path');
+  const chartNumbers = [];
+  const chartVertices = [];
+
+  if (unitSymbol !== undefined && unitType !== undefined) {
+    for (let i = 0; i <= 24; i += 1) {
+      const values = document.createElementNS(svgNs, 'text');
+      const valuesNumber = document.createElementNS(svgNs, 'tspan');
+      const valuesSymbol = document.createElementNS(svgNs, 'tspan');
+      const numbersClasses = [`${className}-chart`, unitType];
+      values.setAttribute('x', 0);
+      values.setAttribute('y', 0);
+      values.setAttribute('text-anchor', 'middle');
+      values.classList.add(`${className}-chart-text`);
+      valuesNumber.classList.add(...numbersClasses);
+      valuesNumber.textContent = '0';
+      valuesSymbol.textContent = `${unitSymbol}`;
+      values.append(valuesNumber, valuesSymbol);
+      chartNumbers.push(values);
+    }
+  }
+
+  for (let i = 0; i <= 24; i += 1) {
+    const vertex = document.createElementNS(svgNs, 'circle');
+    vertex.classList.add(`${className}-chart-vertex`);
+    vertex.setAttribute('r', 4);
+    vertex.setAttribute('cx', 500);
+    vertex.setAttribute('cy', 500);
+    chartVertices.push(vertex);
+  }
+
+  chartGroup.classList.add('hourly-chart-group', `${className}-chart-group`);
+  chart.classList.add(`hourly-chart-${className}`);
+
+  chartGroup.append(chart, ...chartNumbers, ...chartVertices);
+
+  return chartGroup;
+}
--- src/modules/weatherDailyHourly/weatherHourly/weatherHourly.js
+++ src/modules/weatherDailyHourly/weatherHourly/weatherHourly.js
@@ -1,3 +1,5 @@
+import createChartLineGroup from './createChartLineGroup';
+
 const svgNs = 'http://www.w3.org/2000/svg';
 
 export default function weatherHourly() {
@@ -46,45 +48,4 @@ function createChartMarksAndHours() {
   chartLinesGroup.append(horizontalLine, ...hoursTexts);
 
   return chartLinesGroup;
-}
-
-function createChartLineGroup(className, unitSymbol, unitType) {
-  const chartGroup = document.createElementNS(svgNs, 'g');
-  const chart = document.createElementNS(svgNs, 'path');
-  const chartNumbers = [];
-  const chartVertices = [];
-
-  if (unitSymbol !== undefined && unitType !== undefined) {
-    for (let i = 0; i <= 24; i += 1) {
-      const values = document.createElementNS(svgNs, 'text');
-      const valuesNumber = document.createElementNS(svgNs, 'tspan');
-      const valuesSymbol = document.createElementNS(svgNs, 'tspan');
-      const numbersClasses = [`${className}-chart`, unitType];
-      values.setAttribute('x', 0);
-      values.setAttribute('y', 0);
-      values.setAttribute('text-anchor', 'middle');
-      values.classList.add(`${className}-chart-text`);
-      valuesNumber.classList.add(...numbersClasses);
-      valuesNumber.textContent = '0';
-      valuesSymbol.textContent = `${unitSymbol}`;
-      values.append(valuesNumber, valuesSymbol);
-      chartNumbers.push(values);
-    }
-  }
-
-  for (let i = 0; i <= 24; i += 1) {
-    const vertex = document.createElementNS(svgNs, 'circle');
-    vertex.classList.add(`${className}-chart-vertex`);
-    vertex.setAttribute('r', 4);
-    vertex.setAttribute('cx', 500);
-    vertex.setAttribute('cy', 500);
-    chartVertices.push(vertex);
-  }
-
-  chartGroup.classList.add('hourly-chart-group', `${className}-chart-group`);
-  chart.classList.add(`hourly-chart-${className}`);
-
-  chartGroup.append(chart, ...chartNumbers, ...chartVertices);
-
-  return chartGroup;
 }
