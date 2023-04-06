bf802b01b1764f8743e1198beaf549489512ea3b
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-06 19:32:08 -0300

    draw next icon only if weathercode changes

--- src/modules/weatherDailyHourly/weatherHourly/updateChartIcons.js
+++ src/modules/weatherDailyHourly/weatherHourly/updateChartIcons.js
@@ -5,19 +5,29 @@ import getWeathercode from '../../weathercode';
 export default function updateChartIcons(next25Weathercodes, next25Hours, width) {
   const iconsDiv = document.querySelector('.chart-weather-icons-div');
   const icons = document.querySelectorAll('.chart-weather-icon');
+  let previousWeathercode;
 
   iconsDiv.style.width = `${width}px`;
 
   icons.forEach((icon, i) => {
     const iconWidth = width / 23;
-    const iconParts = getWeathercode(next25Weathercodes[i + 1]).icon;
+    const currentWeathercode = next25Weathercodes[i + 1];
+    const iconParts = getWeathercode(currentWeathercode).icon;
     if (i === 0) icon.style.marginLeft = `${iconWidth / 2}px`;
     if (i === icons.length - 1) icon.style.marginRight = `${iconWidth / 2}px`;
-    icon.style.backgroundImage = weatherIcon(
-      iconParts,
-      new Date(next25Hours[i + 1]),
-      setHours(new Date(next25Hours[i + 1]), 6),
-      setHours(new Date(next25Hours[i + 1]), 18),
-    );
+
+    // use icon only if it's different from the previous one
+    if (currentWeathercode !== previousWeathercode) {
+      icon.style.backgroundImage = weatherIcon(
+        iconParts,
+        new Date(next25Hours[i + 1]),
+        setHours(new Date(next25Hours[i + 1]), 6),
+        setHours(new Date(next25Hours[i + 1]), 18),
+      );
+    } else {
+      icon.style.backgroundImage = 'none';
+    }
+
+    previousWeathercode = currentWeathercode;
   });
 }
