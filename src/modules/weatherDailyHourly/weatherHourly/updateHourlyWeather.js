de6b4298c1c3a85ae750a5cfe72a973d59713d09
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-07 17:26:38 -0300

    move all variable declarations to the top

--- src/modules/weatherDailyHourly/weatherHourly/updateHourlyWeather.js
+++ src/modules/weatherDailyHourly/weatherHourly/updateHourlyWeather.js
@@ -26,38 +26,35 @@ export default function updateHourlyWeather(data, startFromIndex) {
   const chartsHeightTemps = ((height * 80) / 100) - textSpace;
   const chartsHeightWindHumidity = height - textSpace;
   const positionsX = [0];
+  // get the necessary data for chart
+  const next25Hours = getNext25(data.hours, startFromIndex);
+  const chartTemp = document.querySelector('.hourly-chart-temp');
+  const chartTempVertices = document.querySelectorAll('.temp-chart-vertex');
+  const next25Temps = getNext25(data.temps, startFromIndex);
+  const next25apparents = getNext25(data.apparent, startFromIndex);
+  const next25WindSpeeds = getNext25(data.windspeed, startFromIndex);
+  const next25Humidity = getNext25(data.humidity, startFromIndex);
+  const next25Weathercodes = getNext25(data.weathercode, startFromIndex);
+  const { sunrise } = data;
+  const { sunset } = data;
+
   // set values for vertex horizontal positions
   for (let i = 0; i < hours; i += 1) {
     const currentValue = (width / (hours)) * (i + 1);
     positionsX.push(currentValue);
   }
-
-  const next25Hours = getNext25(data.hours, startFromIndex);
-  positionLinesAndHours(next25Hours, positionsX, textSpace, width, height);
-  // update current temperature line chart
-  const chartTemp = document.querySelector('.hourly-chart-temp');
-  const chartTempVertices = document.querySelectorAll('.temp-chart-vertex');
+  // if there's no temp chart drawn, an empty one (values at 0) will be placed
   if (!chartTemp.getAttribute('d')) {
     emptyChart(chartTemp, positionsX, chartsHeightTemps);
     emptyChartVertices(chartTempVertices, positionsX, chartsHeightTemps);
   }
   // small timeout to allow animation between the empty chart and the updated one
   setTimeout(() => {
-    const next25Temps = getNext25(data.temps, startFromIndex);
+    positionLinesAndHours(next25Hours, positionsX, textSpace, width, height);
     updateChartLine(next25Temps, positionsX, chartsHeightTemps, hours, textSpace, width, 'temp', '.temperature-number');
-
-    const next25apparents = getNext25(data.apparent, startFromIndex);
     updateChartLine(next25apparents, positionsX, chartsHeightTemps, hours, textSpace, width, 'temp-apparent', '.temperature-number');
-
-    const next25WindSpeeds = getNext25(data.windspeed, startFromIndex);
     updateChartLine(next25WindSpeeds, positionsX, chartsHeightWindHumidity, hours, textSpace, width, 'wind', '.speed-number');
-
-    const next25Humidity = getNext25(data.humidity, startFromIndex);
     updateChartLine(next25Humidity, positionsX, chartsHeightWindHumidity, hours, textSpace, width, 'humidity', '.humidity-percent');
-
-    const next25Weathercodes = getNext25(data.weathercode, startFromIndex);
-    const { sunrise } = data;
-    const { sunset } = data;
     updateChartIcons(next25Weathercodes, sunrise, sunset, next25Hours, width);
   }, 50);
 }
