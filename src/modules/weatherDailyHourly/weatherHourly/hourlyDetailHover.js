7fe61a6c6a100a25a861d4e635877e565b0c510f
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-07 23:02:48 -0300

    reveal vertices only on hovered hour

--- src/modules/weatherDailyHourly/weatherHourly/hourlyDetailHover.js
+++ src/modules/weatherDailyHourly/weatherHourly/hourlyDetailHover.js
@@ -2,6 +2,7 @@ import getWeathercode from '../../weathercode';
 import { celsiusToFahrenheit, kilometersToMiles } from '../../unitConverter';
 
 export function showHourlyDetail() {
+  const pointed = parseInt(this.dataset.pointed, 10);
   const detailsCard = document.querySelector('.chart-details-div');
   detailsCard.classList.remove('hidden');
   // unit checkboxes
@@ -16,6 +17,11 @@ export function showHourlyDetail() {
   const windUnit = document.querySelector('.chart-details-unit-wind');
   const humidity = document.querySelector('.chart-details-value-humidity');
   const weather = document.querySelector('.chart-details-value-weathercode');
+  // vertices
+  const tempVertices = document.querySelectorAll('.temp-chart-vertex');
+  const windVertices = document.querySelectorAll('.wind-chart-vertex');
+  const humidityVertices = document.querySelectorAll('.humidity-chart-vertex');
+  const tempApparentVertices = document.querySelectorAll('.temp-apparent-chart-vertex');
   // weather description
   const weatherType = getWeathercode(parseInt(this.dataset.weathercode, 10)).weather;
   const weatherIntensity = getWeathercode(parseInt(this.dataset.weathercode, 10))?.intensity;
@@ -30,9 +36,39 @@ export function showHourlyDetail() {
   windUnit.innerText = speedUnits.checked ? 'mph' : 'km/h';
   humidity.innerText = this.dataset.humidity;
   weather.innerText = `${weatherType}${weatherIntensity ? `, ${weatherIntensity}` : ''}`;
+
+  const allVertices = [tempVertices, windVertices, humidityVertices, tempApparentVertices];
+  allVertices.forEach((vertices) => {
+    vertices.forEach((vertex, i) => {
+      if (pointed === i) {
+        vertex.classList.remove('hidden');
+        vertex.setAttribute('r', 7);
+      } else {
+        vertex.classList.add('hidden');
+        vertex.setAttribute('r', 4);
+      }
+    });
+  });
 }
 
 export function hideHourlyDetail() {
   const detailsCard = document.querySelector('.chart-details-div');
+  // vertices
+  const tempVertices = document.querySelectorAll('.temp-chart-vertex');
+  const windVertices = document.querySelectorAll('.wind-chart-vertex');
+  const humidityVertices = document.querySelectorAll('.humidity-chart-vertex');
+  const tempApparentVertices = document.querySelectorAll('.temp-apparent-chart-vertex');
+
   detailsCard.classList.add('hidden');
+  const allVertices = [tempVertices, windVertices, humidityVertices, tempApparentVertices];
+  allVertices.forEach((vertices) => {
+    vertices.forEach((vertex) => {
+      if (vertex.dataset.hidden === 'true') {
+        vertex.classList.add('hidden');
+      } else {
+        vertex.classList.remove('hidden');
+      }
+      vertex.setAttribute('r', 4);
+    });
+  });
 }
