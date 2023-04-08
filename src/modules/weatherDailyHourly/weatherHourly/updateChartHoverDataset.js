61b2cc4bcca1115d68533a5a18ecf45f7e61a578
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-07 21:53:28 -0300

    remove hour from argument and dataset

--- src/modules/weatherDailyHourly/weatherHourly/updateChartHoverDataset.js
+++ src/modules/weatherDailyHourly/weatherHourly/updateChartHoverDataset.js
@@ -1,5 +1,4 @@
 export default function updateChartHoverDataset(
-  hours,
   temps,
   apparents,
   windSpeeds,
@@ -8,14 +7,13 @@ export default function updateChartHoverDataset(
 ) {
   const chartHover = document.querySelectorAll('.chart-weather-icon');
   // removing unnecesary first and last elements
-  [hours, temps, apparents, windSpeeds, humidity, weathercodes]
+  [temps, apparents, windSpeeds, humidity, weathercodes]
     .forEach((data) => {
       data.shift();
       data.pop();
     });
   chartHover.forEach((item, i) => {
     // as the first div starts from the 2nd mark, the first element is discarded
-    item.dataset.hour = hours[i];
     item.dataset.temp = temps[i];
     item.dataset.apparent = apparents[i];
     item.dataset.wind = windSpeeds[i];
