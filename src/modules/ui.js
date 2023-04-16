0e349707563b0a3544d5cd7dd2d8d7d0bd22f5e1
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-15 22:37:38 -0300

    import and append background module

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -2,10 +2,12 @@ import search from './search/search';
 import weatherCurrent from './weatherCurrent/weatherCurrent';
 import weatherDailyHourly from './weatherDailyHourly/weatherDailyHourly';
 import updateHourlyWeather from './weatherDailyHourly/weatherHourly/updateHourlyWeather';
+import createBackground from './background/background';
 
 export default function loadUi() {
   const body = document.querySelector('body');
   body.append(
+    createBackground(),
     search(),
     weatherCurrent(),
     weatherDailyHourly(),
