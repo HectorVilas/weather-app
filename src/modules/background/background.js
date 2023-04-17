94e45e9a08cbae4da3557ffb3919a5564d543e03
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-17 20:32:12 -0300

    add divs for the background's curtains

--- src/modules/background/background.js
+++ src/modules/background/background.js
@@ -1,11 +1,22 @@
 export default function createBackground() {
   const section = document.createElement('section');
   const window = document.createElement('div');
+  const curtains = document.createElement('div');
+  const curtainFolds = 10;
 
-  window.classList.add('bg-window');
+  for (let i = 0; i < curtainFolds; i += 1) {
+    const curtain = document.createElement('div');
+    const className = i < curtainFolds / 2 ? 'bg-curtain-left' : 'bg-curtain-right';
+    curtain.classList.add('bg-curtain', className);
+    curtains.append(curtain);
+  }
+
   section.classList.add('background');
+  window.classList.add('bg-window');
+  curtains.classList.add('bg-curtains');
 
   section.append(
+    curtains,
     window,
   );
 
