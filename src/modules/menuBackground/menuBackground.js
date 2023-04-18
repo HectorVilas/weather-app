a8f780c8d7b7eba85bc796073575ca7364641898
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2023-04-18 16:22:57 -0300

    move module to own folder

--- src/modules/menuBackground/menuBackground.js
+++ src/modules/menuBackground/menuBackground.js
@@ -1,4 +1,4 @@
-export default function mainMenuBackground() {
+export default function menuBackground() {
   const section = document.createElement('section');
 
   section.classList.add('main-menu-background');
